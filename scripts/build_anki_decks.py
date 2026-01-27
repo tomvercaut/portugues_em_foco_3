d58dabe4c2725208b4e7be443fe138b039af7782
Tom <tomvercaut@gmail.com>
2026-01-28 00:23:29 +0100

    Modified build Anki decks script.

--- scripts/build_anki_decks.py
+++ scripts/build_anki_decks.py
@@ -7,16 +7,19 @@ from pathlib import Path
 
 def get_typ_files(p: Path):
     l = {}
-    for (_dirpath, _dirnames, filenames) in os.walk(p):
+    for (dirpath, _dirnames, filenames) in os.walk(p):
         for f in filenames:
             _, extention = os.path.splitext(f)
             if extention != '.typ':
                 continue
             filename = Path(f).stem
-            if filename == 'unidade_x' or filename == 'main' \
-                or filename == 'common' or filename == "preamble":
+            if not filename.startswith('unidade_'):
                 continue
-            l[filename] = f
+            if filename == 'unidade_x':
+                continue
+            fpath = os.path.join(dirpath, filename)
+            # print(f"Typ file: {fpath}")
+            l[filename] = f"{fpath}.typ"
     return l
 
 if __name__ == '__main__':
@@ -27,12 +30,14 @@ if __name__ == '__main__':
     parser.add_argument('input', type=str, help='Input directory')
     parser.add_argument('output', type=str, help='Output directory')
     args = parser.parse_args()
-    files = get_typ_files(args.input)
+    input = args.input
+    files = get_typ_files(input)
 
     for k, v in files.items():
-        print(f"{k}: {v}")
-        ifile=os.path.join(args.input, v)
-        ofile=os.path.join(args.output, k + '.csv')
+        ifile=os.path.join(input, f"{v}")
+        ofilename = k.replace('/', '_')
+        ofilename = ofilename.replace('\\', '_')
+        ofile=os.path.join(args.output, ofilename + '.csv')
         deck='português::foco3::'+k
         subprocess.run(['voc_to_anki.py', ifile, ofile, '--deck', deck])
 
