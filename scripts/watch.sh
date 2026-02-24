44d48b1a777209745963897efab87c62b37a5aed
Tom <tomvercaut@gmail.com>
2026-02-24 13:09:16 +0100

    script: update vocabulary for Anki automatically.

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 typst watch main.typ out/portugues_em_foco_3.pdf &
+ls ./out/portugues_em_foco_3.pdf | entr ./scripts/build_anki_decks.py . ./anki/
 typst watch --input paper=a6 main.typ out/portugues_em_foco_3_mobile.pdf
